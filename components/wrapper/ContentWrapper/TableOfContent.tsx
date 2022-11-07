cbf57771bffdce971a730607b559147b8d8b766d
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2022-11-07 14:51:31 +0530

    Created content wrapper

---
+++ components/wrapper/ContentWrapper/TableOfContent.tsx
@@ -0,0 +1,13 @@
+import { VStack, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export default function TableOfContent() {
+  return (
+    <VStack>
+        <Text as='b'>ON THIS PAGE</Text>
+        <Text>Framework Guide</Text>
+        <Text>Learn</Text>
+        <Text>Contribute</Text>
+    </VStack>
+  )
+}
