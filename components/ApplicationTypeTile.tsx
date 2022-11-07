e53da901156ad425ab5121f6b39fd70504ab077d
Aditya Mathur <mathuraditya724@gmail.com>
2022-11-07 16:05:12 +0530

    working on the home page

---
+++ components/ApplicationTypeTile.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ApplicationTypeTile() {
+  return <div>ApplicationTypeTile</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
+import { Box, Container, Heading, HStack, Text } from "@chakra-ui/react";
+import ApplicationTypeTile from "components/ApplicationTypeTile";
 import DefaultWrapper from "components/wrapper/DefaultWrapper";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Container maxW="4xl" my={5}>
+      <Box my={20}>
+        <Heading>Start Building</Heading>
+        <Text fontSize="2xl" mb={10}>
+          Choose your application type to get started
+        </Text>
+      </Box>
+      <HStack>
+        <ApplicationTypeTile />
+        <ApplicationTypeTile />
+        <ApplicationTypeTile />
+        <ApplicationTypeTile />
+      </HStack>
+    </Container>
+  );
 }
 
 Home.getLayout = function getLayout(page: JSX.Element) {
