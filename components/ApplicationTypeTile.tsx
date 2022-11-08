407c0a82817a4934195af2d6fa03b007dc29253f
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2022-11-08 11:50:07 +0530

    Created application type tile for the home page

--- components/ApplicationTypeTile.tsx
+++ components/ApplicationTypeTile.tsx
@@ -1,5 +1,46 @@
+import { Button, Flex, Image, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-export default function ApplicationTypeTile() {
-  return <div>ApplicationTypeTile</div>;
+type ApplicationTypeTileProps = {
+  image: string;
+  title: string;
+  content: string;
+  example: string;
+};
+
+export default function ApplicationTypeTile(props: ApplicationTypeTileProps) {
+  return (
+    <Flex
+      h="320px"
+      flexDir="column"
+      alignItems="center"
+      border="4px"
+      borderColor="transparent"
+      _hover={{ borderColor: "messenger.200" }}
+      justifyContent="center"
+      boxShadow="0px 2px 4px rgb(0 0 0 / 12%)"
+      borderRadius="md"
+      p={7}
+      transition="border 0.1s ease 0s"
+      bgColor="white"
+    >
+      <Image
+        borderRadius="2xl"
+        boxSize="100px"
+        src={props.image}
+        alt={props.title}
+        boxShadow="lg"
+        mb={7}
+      />
+      <Text textAlign="center" as="b" fontSize="lg" mb={2}>
+        {props.title}
+      </Text>
+      <Text textAlign="center" noOfLines={2} mb={1}>
+        {props.content}
+      </Text>
+      <Text fontSize="13px" noOfLines={1} color="GrayText" mt={1}>
+        {props.example}
+      </Text>
+    </Flex>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,59 @@
-import { Box, Container, Heading, HStack, Text } from "@chakra-ui/react";
+import { Box, Container, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import ApplicationTypeTile from "components/ApplicationTypeTile";
 import DefaultWrapper from "components/wrapper/DefaultWrapper";
 
 export default function Home() {
   return (
-    <Container maxW="4xl" my={5}>
-      <Box my={20}>
-        <Heading>Start Building</Heading>
-        <Text fontSize="2xl" mb={10}>
-          Choose your application type to get started
-        </Text>
-      </Box>
-      <HStack>
-        <ApplicationTypeTile />
-        <ApplicationTypeTile />
-        <ApplicationTypeTile />
-        <ApplicationTypeTile />
-      </HStack>
-    </Container>
+    <Box py={5} pos="relative">
+      <Box
+        pos="absolute"
+        w="100%"
+        h="90%"
+        bgColor="gray.100"
+        top={0}
+        left={0}
+        borderBottomRadius="40px"
+        zIndex="-1"
+      />
+      <Container maxW="7xl">
+        <Box my={20}>
+          <Heading>Start Building</Heading>
+          <Text fontSize="2xl" mb={10}>
+            Choose your application type to get started
+          </Text>
+        </Box>
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 4 }}
+          spacing={10}
+          mx={{ base: 5, lg: 0 }}
+        >
+          <ApplicationTypeTile
+            image="https://via.placeholder.com/150?text=%20"
+            title="Native/Mobile App"
+            content="Mobile or Desktop app that runs natively on a device"
+            example="e.g., IOS, Andriod"
+          />
+          <ApplicationTypeTile
+            image="https://via.placeholder.com/150?text=%20"
+            title="Single-Page App"
+            content="JavaScript web app that runs in the browser"
+            example="e.g., AngularJS + Node.js, React"
+          />
+          <ApplicationTypeTile
+            image="https://via.placeholder.com/150?text=%20"
+            title="Regular Web App"
+            content="Traditional web app that runs on the server"
+            example="e.g., Express.js, ASP.NET"
+          />
+          <ApplicationTypeTile
+            image="https://via.placeholder.com/150?text=%20"
+            title="Backend/API"
+            content="An API or service protected by Auth0"
+            example="e.g., Express.js API, ASP.NET API"
+          />
+        </SimpleGrid>
+      </Container>
+    </Box>
   );
 }
 
