cd9f93db3abdd36b2e939db81cdfc76cb291dd6c
Sarkarraghu <117056558+Sarkarraghu@users.noreply.github.com>
2022-11-08 15:57:32 +0530

    completed the QuickstartTile Component

--- components/QuickstartTile.tsx
+++ components/QuickstartTile.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import { Heading, Image, HStack } from "@chakra-ui/react";
+import Link from "next/link";
 
 export type QuickstartTileProps = {
-  title: "string";
-  href: "string";
-  image: "string";
+  title: string;
+  href: string;
+  image: string;
 };
 
-export default function QuickstartTile(this: QuickstartTileProps) {
-  return <></>;
+export default function QuickstartTile(props: QuickstartTileProps) {
+  return (
+    <Link href={props.href}>
+      <HStack
+        h="72px"
+        border="1px"
+        borderColor="GrayText"
+        borderRadius="md"
+        p={7}
+      >
+        <Image src={props.image} alt={props.title} boxSize="50px" />
+        <Heading size="md" fontWeight={500}>
+          {props.title}
+        </Heading>
+      </HStack>
+    </Link>
+  );
 }
--- pages/quickstart/index.tsx
+++ pages/quickstart/index.tsx
@@ -7,7 +7,7 @@ import {
   Text,
   Image,
 } from "@chakra-ui/react";
-import { QuickstartTileProps } from "components/QuickstartTile";
+import QuickstartTile, { QuickstartTileProps } from "components/QuickstartTile";
 import React from "react";
 
 type QuickstartType = {
@@ -22,7 +22,28 @@ const data: QuickstartType[] = [
     title: "Regular Web App",
     description: "Traditional web app that runs on the server",
     image: "https://via.placeholder.com/150?text=%20",
-    items: [],
+    items: [
+      {
+        title: "Python",
+        href: "",
+        image: "https://via.placeholder.com/150?text=%20",
+      },
+      {
+        title: "Javascript",
+        href: "",
+        image: "https://via.placeholder.com/150?text=%20",
+      },
+      {
+        title: "PHP",
+        href: "",
+        image: "https://via.placeholder.com/150?text=%20",
+      },
+      {
+        title: "Java",
+        href: "",
+        image: "https://via.placeholder.com/150?text=%20",
+      },
+    ],
   },
 ];
 
@@ -42,9 +63,15 @@ export default function Quickstarts() {
                 />
                 <Heading>{value.title}</Heading>
               </HStack>
-              <Text fontSize="2xl" my={5}>{value.description}</Text>
+              <Text fontSize="2xl" my={5}>
+                {value.description}
+              </Text>
             </Box>
-            <SimpleGrid columns={{base: 1, md: 2, lg: 3}}></SimpleGrid>
+            <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={5}>
+              {value.items.map((item, index) => (
+                <QuickstartTile key={index} {...item} />
+              ))}
+            </SimpleGrid>
           </React.Fragment>
         ))}
       </Container>
