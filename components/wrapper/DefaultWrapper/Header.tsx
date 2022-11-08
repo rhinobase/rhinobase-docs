9c9d1b18224ebbd02426d4754313d2b13907c685
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2022-11-08 11:50:25 +0530

    small changes

--- components/wrapper/DefaultWrapper/Header.tsx
+++ components/wrapper/DefaultWrapper/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
-    <HStack m={3}>
+    <HStack py={3}>
       <Link href="/">
         <FullLogo logo={{ size: 40, borderRadius: "md", p: 1 }} size="lg" />
       </Link>
@@ -41,7 +41,7 @@ export default function Header() {
         onClick={toggleColorMode}
         variant="ghost"
         aria-label="togglemode"
-        icon={<SwitchIcon />}
+        icon={<SwitchIcon size="20px" />}
       />
     </HStack>
   );
