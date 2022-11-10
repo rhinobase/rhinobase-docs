7bb203e0f811c155f8559e7543a37cb47170b876
Sarkarraghu <117056558+Sarkarraghu@users.noreply.github.com>
2022-11-10 14:33:58 +0530

    Removed comment

--- components/content/ContentWrapper.tsx
+++ components/content/ContentWrapper.tsx
@@ -52,16 +52,3 @@ export default function ContentWrapper(props: ContentWrapperProps) {
     </>
   );
 }
-
-{
-  /* <Show breakpoint='(max-width: 400px)'>
-  <Box>This text appears only on screens 400px and smaller.</Box>
-</Show> */
-}
-
-//   <Show above='sm'>
-//     <Box>This text appears at the "sm" value screen width or greater.</Box>
-//   </Show>
-//   <Hide below='md'>
-//     <Box>This text hides at the "md" value screen width and smaller.</Box>
-//   </Hide>
