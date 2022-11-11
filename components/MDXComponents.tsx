1a489b0a3ce90f134d299176367c6c2c32a1ee89
Sarkarraghu <117056558+Sarkarraghu@users.noreply.github.com>
2022-11-11 15:29:46 +0530

    Update MDXComponents.tsx

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,8 +1,16 @@
-import { Code, Heading } from "@chakra-ui/react";
+import { Code, Heading, Text, ListItem, OrderedList, UnorderedList } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
 
 const MDXComponents = {
   code: Code,
-  h2: Heading
+  h2: Heading,
+  p: Text,
+  ul: UnorderedList,
+  ol: OrderedList,
+  li: ListItem,
+  img: Image,
+  a: Link,
 };
 
 export default MDXComponents;
