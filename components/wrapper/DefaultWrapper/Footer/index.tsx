fc0a71c09cfc5d10f2fc8ffd714bfede8a6cff58
Sarkarraghu <117056558+Sarkarraghu@users.noreply.github.com>
2022-11-14 12:28:17 +0530

    Updated the footer

--- components/wrapper/DefaultWrapper/Footer/index.tsx
+++ components/wrapper/DefaultWrapper/Footer/index.tsx
@@ -6,81 +6,97 @@ import {
   Spacer,
   Button,
   ButtonGroup,
+  IconButton,
+  GridItem,
+  Box,
+  Stack,
 } from "@chakra-ui/react";
+import {
+  FaGithub,
+  FaDiscord,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+  FaFacebook,
+  FaTwitch,
+} from "react-icons/fa";
 import FooterList, { FooterListProps } from "./FooterList";
+import Logo from "components/brand/Logo";
+import Link from "next/link";
 
 const data: FooterListProps[] = [
   {
-    title: "Platform",
+    title: "Company",
     items: [
       {
-        title: "Access Management",
+        title: "About Us",
         href: "",
       },
       {
-        title: "Extensibility",
+        title: "Careers",
         href: "",
       },
       {
-        title: "Login Security",
+        title: "Articles",
+        href: "https://www.rhinobase.io/articles",
+      }
+    ],
+  },
+  {
+    title: "Product",
+    items: [
+      {
+        title: "CLI",
         href: "",
       },
       {
-        title: "User Management",
+        title: "Customers",
         href: "",
       },
       {
-        title: "Authentication",
+        title: "Enterprise",
+        href: "",
+      },
+      {
+        title: "Pricing",
+        href: "",
+      },
+      {
+        title: "Changelog",
         href: "",
       },
     ],
   },
   {
-    title: "Use Cases",
+    title: "Resources",
     items: [
       {
-        title: "CIAM",
-        href: "",
-        description: "For your Customers",
-      },
-      {
-        title: "B2B",
-        href: "",
-        description: "For your Business Partner",
-      },
-      {
-        title: "B2E",
-        href: "",
-        description: "For your Employee",
-      },
-    ],
-  },
-  {
-    title: "Platform",
-    items: [
-      {
-        title: "Access Management",
+        title: "Docs",
         href: "",
       },
       {
-        title: "Extensibility",
+        title: "Support",
+        href: "",
+      },
+      {
+        title: "Status",
+        href: "",
+      },
+      {
+        title: "Contact",
         href: "",
       },
     ],
   },
   {
-    title: "Platform",
+    title: "Talk to us",
     items: [
       {
-        title: "Access Management",
+        title: "Call +91",
         href: "",
       },
       {
-        title: "Extensibility",
-        href: "",
-      },
-      {
-        title: "Login Security",
+        title: "Contact Sales",
         href: "",
       },
     ],
@@ -93,29 +109,69 @@ export default function Footer() {
       <Grid
         templateColumns={{
           base: "repeat(1, 1fr)",
-          md: "repeat(2, 1fr)",
-          lg: "repeat(4, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(3, 1fr)",
+          lg: "repeat(5, 1fr)",
         }}
         gap={2}
-        h="max-content"
         my={5}
       >
+        <GridItem w="100%" p={3} colSpan={{ base: 1, sm: 2, md: 1 }}>
+          <Link href="/">
+            <Logo size={40} borderRadius="8" p={1} />
+          </Link>
+        </GridItem>
+
         {data.map((item, index) => (
           <FooterList key={index} {...item} />
         ))}
       </Grid>
       <Divider my={4} />
-      <HStack mb={4}>
-        <Text color="GrayText">
-          © 2020 - {new Date().getFullYear()} Rhinobase® Inc. All Rights
-          Reserved.
-        </Text>
+      <HStack
+        mb={4}
+        color="GrayText"
+        gap={3}
+        flexDir={{ base: "column-reverse", lg: "row" }}
+      >
+        <HStack flexDir={{ base: "column", lg: "row" }} gap={4}>
+          <Text textAlign="center">
+            © 2020 - {new Date().getFullYear()} Rhinobase® Inc. All Rights
+            Reserved.
+          </Text>
+          <HStack>
+          <Link href={""}>Privacy</Link>
+          <Link href={""}>Terms</Link>
+          </HStack>
+        </HStack>
         <Spacer />
-        <ButtonGroup variant="link">
-          <Button>Status</Button>
-          <Button>Legal</Button>
-          <Button>Privacy</Button>
-          <Button>Terms</Button>
+
+        <ButtonGroup spacing={1}>
+          <IconButton
+            variant="ghost"
+            aria-label="twitter"
+            icon={<FaTwitter />}
+          />
+          <IconButton variant="ghost" aria-label="github" icon={<FaGithub />} />
+          <IconButton
+            variant="ghost"
+            aria-label="linkedin"
+            icon={<FaLinkedin />}
+          />
+          <IconButton
+            variant="ghost"
+            aria-label="facebook"
+            icon={<FaFacebook />}
+          />
+          <IconButton
+            variant="ghost"
+            aria-label="youtube"
+            icon={<FaYoutube />}
+          />
+          <IconButton
+            variant="ghost"
+            aria-label="discord"
+            icon={<FaDiscord />}
+          />
         </ButtonGroup>
       </HStack>
     </>
